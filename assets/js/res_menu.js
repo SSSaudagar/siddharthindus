3237e9e14250b04ba4f41782bf38693aeaa8d374
Priya Tyagi <priya@leiansis.com>
2015-06-08 19:09:34 +0530

    minor changes made w.r.t responsive menu

--- assets/js/res_menu.js
+++ assets/js/res_menu.js
@@ -1,6 +1,10 @@
         $(document).ready(function() {
     $('.toggle-nav').click(function(e) {
-         $('.nav_bar ul').toggleClass('active');
+         $('.nav_bar ul').removeClass('active');
+        e.preventDefault();
+    });
+    $('.menu_icn').click(function(e) {
+         $('.nav_bar ul').addClass('active');
         e.preventDefault();
     });       
 });
