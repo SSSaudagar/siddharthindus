36a2fe5c8db9bddbf7f425ed7d57d52dc5109c45
Priya Tyagi <priya@leiansis.com>
2015-09-17 02:22:02 +0530

    minor

--- assets/js/navbr.js
+++ assets/js/navbr.js
@@ -1,26 +1,25 @@
-    $(document).ready(function(){
-        var nav_ofst = $(".nav_br").offset().top;
-        console.log(nav_ofst);
-        $(window).scroll(function(){
-        var scroltp = $(window).scrollTop();
-        var ofst_win = nav_ofst - scroltp;
-        if(ofst_win<0){
-        $(".nav_br").addClass("active");
-        $(".nav_br ul").addClass("active");
-        $("span#comp_nm_nv").addClass("active");
-        $(".nav_br span#menu_icon").addClass("active");
-        }
-        else{
-            $(".nav_br").removeClass("active");
-            $(".nav_br ul").removeClass("active");
-            $("span#comp_nm_nv").removeClass("active");
-            $(".nav_br span#menu_icon").removeClass("active");
-        }
-        });
-        $("span#menu_icon").click(function(){
-        $(".nav_br ul").addClass("active1");
-        });
-        $(".nav_br ul li#menu_list_icon").click(function(){
-        $(".nav_br ul").removeClass("active1");
-        });
-    });
+$(document).ready(function(){
+var nav_ofst = $(".nav_br").offset().top;
+$(window).scroll(function(){
+var scroltp = $(window).scrollTop();
+var ofst_win = nav_ofst - scroltp;
+if(ofst_win<0){
+$(".nav_br").addClass("active");
+$(".nav_br ul").addClass("active");
+$("span#comp_nm_nv").addClass("active");
+$(".nav_br span#menu_icon").addClass("active");
+}
+else{
+    $(".nav_br").removeClass("active");
+    $(".nav_br ul").removeClass("active");
+    $("span#comp_nm_nv").removeClass("active");
+    $(".nav_br span#menu_icon").removeClass("active");
+}
+});
+$("span#menu_icon").click(function(){
+$(".nav_br ul").addClass("active1");
+});
+$(".nav_br ul li#menu_list_icon").click(function(){
+$(".nav_br ul").removeClass("active1");
+});
+});
