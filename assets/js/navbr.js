0017ce44e54ab0e49c02c8b85f14368699262589
Priya Tyagi <priya@leiansis.com>
2015-09-17 01:42:02 +0530

    responsive menu

--- assets/js/navbr.js
+++ assets/js/navbr.js
@@ -1,5 +1,4 @@
     $(document).ready(function(){
-        
         var nav_ofst = $(".nav_br").offset().top;
         console.log(nav_ofst);
         $(window).scroll(function(){
@@ -9,11 +8,19 @@
         $(".nav_br").addClass("active");
         $(".nav_br ul").addClass("active");
         $("span#comp_nm_nv").addClass("active");
+                $(".nav_br span#menu_icon").addClass("active");
         }
         else{
             $(".nav_br").removeClass("active");
             $(".nav_br ul").removeClass("active");
             $("span#comp_nm_nv").removeClass("active");
+            $(".nav_br span#menu_icon").removeClass("active");
         }
+        });
+        $("span#menu_icon").click(function(){
+        $(".nav_br ul").addClass("active1");
+        });
+        $(".nav_br ul li#menu_list_icon").click(function(){
+        $(".nav_br ul").removeClass("active1");
         });
     });
