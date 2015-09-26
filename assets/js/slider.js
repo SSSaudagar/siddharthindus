49a96b1cf4d33781725bc6339e695405f04d9de1
Priya Tyagi <priya@leiansis.com>
2015-09-26 23:43:24 +0530

    2 diff sets of corousel wid 2 diff writeup in tab1 done

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,4 +1,4 @@
-        $(document).ready(function(){
+$(document).ready(function(){
 var indicatr = $('ul.slider_indicator li');
 var imags = $('.pic_slidr ul li');
 var lastElem = indicatr.length-1;
@@ -22,8 +22,17 @@ indicatr.click(function() {
 
 function sliderTiming() {
     tar = $('ul.slider_indicator li.active').index();
+	console.log('index is:'+tar);
     tar === lastElem ? tar = 0 : tar = tar+1;
     sliderResponse(tar);
+	if(tar>3){
+		$(".extrusion_cont").removeClass("active");
+		$("#extrusion2_content").addClass("active");
+	}
+	else{
+		$(".extrusion_cont").removeClass("active");
+		$("#extrusion1_content").addClass("active");
+	}
 }
 
 var timingRun = setInterval(function() { sliderTiming(); },4000);
